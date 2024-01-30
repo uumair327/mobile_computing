c8033ddc55228d55223fe2f78c6e07d86028471e
uumair327 <mohdumair.a@somaiya.edu>
2024-01-30 22:23:33 +0530

    mobile computing sign in

---
+++ android/app/src/main/kotlin/com/example/mobile_computing/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mobile_computing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:mobile_computing/screens/signin_screen.dart';
+
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions
+        .currentPlatform, // Use this for default configuration
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+      ),
+      home: const SignInScreen(),
+    );
+  }
+}
